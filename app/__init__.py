1d21b1f77bcd2d1ace414af9491ea405f20d03ad
chnihy <69132469+chnihy@users.noreply.github.com>
2022-05-27 14:28:23 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
-
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
 app.debug = True
+
+# initialize bootstrap
+bootstrap = Bootstrap(app)
 
 from app import routes
